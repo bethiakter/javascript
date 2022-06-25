81ad4d72be997b41bffcf65ae076e4867fc23d58
Bethi Akter <96329084+bethiakter@users.noreply.github.com>
2022-06-25 12:03:32 +0600

    files adeed

---
+++ JavaScript/CGPA.js
@@ -0,0 +1,14 @@
+const x = document.getElementById("button");
+
+
+function myFunction(){
+    const y = document.getElementById("input").value;
+    if(y=="A+"){
+      const z = document.getElementById("para");
+      z.innerHTML = "4.00";
+   
+    }
+    
+
+}
+
